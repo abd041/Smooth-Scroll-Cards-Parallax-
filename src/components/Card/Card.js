f3a1d0bed54576f13445e57d3786c952aeaf457c
Muhammad Abdullah <mabdullah@ogoul.com>
2024-11-06 05:09:47 +0500

    cards updated

---
+++ src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Card.css"
+
+const Card = ({title , description , src , link , color}) => {
+  return (
+    <div className='card-main-wrapper'>
+        <div className='card-wrapper' style={{background:color}}>
+    
+    </div>
+    </div>
+    
+  )
+}
+
+export default Card
